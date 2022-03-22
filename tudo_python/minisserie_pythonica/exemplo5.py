7fb76d652ba66c134b3b6452d6cc70fba9228ae0
Luan Bezerra <luan.silvapvh@gmail.com>
2022-03-22 15:05:30 -0400

    Repositorio dedicado aos estudos de python

---
+++ tudo_python/minisserie_pythonica/exemplo5.py
@@ -0,0 +1,26 @@
+# Documentação de funcoes
+# Periodo é um .
+from typing import NewType
+
+
+Radiano = NewType('Radiano', int)
+
+
+def calc_radianus_triangulo(x: int, y: int) -> int:
+    # PEP - 257 
+    """Soma 2 tipos somáveis.
+
+    NOTE: Se voce ver isso no futuro, saiba que foi documentado 
+    TODO: arruma a docstring
+    FIXED:
+
+    :args:
+        x: Objeto somavel
+        y: Objeto somavel
+    
+    :returns: Soma de dois somaveis
+
+    """
+
+
+    
