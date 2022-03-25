41f6f93bd729deb1f07732b7f25b4877d49ce492
Luan Bezerra <luan.silvapvh@gmail.com>
2022-03-25 11:20:33 -0400

    exemplo da Live de python #61

---
+++ programacao_orientada_objetos/exemplo1.py
@@ -0,0 +1,14 @@
+# Abstracao
+# tipos definidos pelo usuario 
+
+
+class Fila:
+    
+    def __init__(self):
+       self.fila = []
+
+    def entrar(self, nome):
+        self.fila.append(nome)    
+
+    def sair(self):
+        self.fila.pop(0)
