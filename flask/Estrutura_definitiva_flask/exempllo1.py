bcff6b0e64be82af3abbe3883f296032cd667840
Luan Bezerra <luan.silvapvh@gmail.com>
2022-04-29 11:34:31 -0400

    Estrutura MVC e factory pattern

---
+++ flask/Estrutura_definitiva_flask/exempllo1.py
@@ -0,0 +1,16 @@
+'''
+Com o flask voce pode começar pequeno e terminar grande
+não há maneira certa de fazer, mas existem principios
+
+#Minimalismo = pode começar pequenno
+Extensibilidade = Pode ser grande
+Liberdade = Liberdade para escolher
+Performance = muito rapido para a maioria dos projetos 
+Robustez = dificilmente quebra
+padrão = de escrita e arquitetura
+
+Principais problemas:
+Thread Locals = app contexts
+Propensão a circular imports
+Extensoes nao oficiais e dispersas 
+'''
--- flask/Estrutura_flask_mvc/app/__init__.py
+++ flask/Estrutura_flask_mvc/app/__init__.py
@@ -1,12 +1,17 @@
 #Definindo a aplicação, esse é o modulo principal
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__) #Variavel especial, usada para transformar em um modulo python
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db' #Caminho do banco de dados para conexao
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)#Migrando o banco de dados, minha aplicacao e o db
 
-from app.controllers import default
+manager = Manager(app) #Comando para inicializar, vai cuidar dos comandos e por padrão ja vem com ru server
+manager.add_command('db', MigrateCommand)
+
 
     
 
